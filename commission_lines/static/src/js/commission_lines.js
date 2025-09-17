d093d4e4e728e063ad46595a87c25667351022ef
scholarix <139387428+renbran@users.noreply.github.com>
2025-09-18 01:25:50 +0400

    g

--- commission_lines/static/src/js/commission_lines.js
+++ commission_lines/static/src/js/commission_lines.js
@@ -4,32 +4,34 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 
 export class CommissionLinesWidget extends Component {
-    static template = "commission_lines.CommissionWidget";
-    static props = {
-        options: { type: Object, optional: true },
-    };
+  static template = "commission_lines.CommissionWidget";
+  static props = {
+    options: { type: Object, optional: true },
+  };
 
-    setup() {
-        this.options = this.props.options || {};
-    }
+  setup() {
+    this.options = this.props.options || {};
+  }
 
-    mounted() {
-        this._bindEvents();
-    }
+  mounted() {
+    this._bindEvents();
+  }
 
-    _bindEvents() {
-        const calculateBtn = this.el.querySelector(".commission-calculate");
-        if (calculateBtn) {
-            calculateBtn.addEventListener("click", () => {
-                this._calculateCommission();
-            });
-        }
+  _bindEvents() {
+    const calculateBtn = this.el.querySelector(".commission-calculate");
+    if (calculateBtn) {
+      calculateBtn.addEventListener("click", () => {
+        this._calculateCommission();
+      });
     }
+  }
 
-    _calculateCommission() {
-        // Commission calculation logic here
-        console.log("Calculating commission...");
-    }
+  _calculateCommission() {
+    // Commission calculation logic here
+    console.log("Calculating commission...");
+  }
 }
 
-registry.category("components").add("CommissionLinesWidget", CommissionLinesWidget);
+registry
+  .category("components")
+  .add("CommissionLinesWidget", CommissionLinesWidget);
