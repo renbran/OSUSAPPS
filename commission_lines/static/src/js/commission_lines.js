55c2238714ce05e618c175476969cef9a4d357c0
scholarix <139387428+renbran@users.noreply.github.com>
2025-09-17 20:18:23 +0400

    g

--- commission_lines/static/src/js/commission_lines.js
+++ commission_lines/static/src/js/commission_lines.js
@@ -1,37 +1,36 @@
-odoo.define('commission_lines.commission_lines_widget', function (require) {
-'use strict';
+odoo.define("commission_lines.commission_lines_widget", function (require) {
+  "use strict";
 
-var Widget = require('web.Widget');
-var core = require('web.core');
+  var Widget = require("web.Widget");
+  var core = require("web.core");
 
-var CommissionLinesWidget = Widget.extend({
-    template: 'commission_lines.CommissionWidget',
-    
-    init: function(parent, options) {
-        this._super(parent);
-        this.options = options || {};
+  var CommissionLinesWidget = Widget.extend({
+    template: "commission_lines.CommissionWidget",
+
+    init: function (parent, options) {
+      this._super(parent);
+      this.options = options || {};
     },
-    
-    start: function() {
-        var self = this;
-        return this._super().then(function() {
-            self._bindEvents();
-        });
-    },
-    
-    _bindEvents: function() {
-        var self = this;
-        this.$('.commission-calculate').on('click', function() {
-            self._calculateCommission();
-        });
-    },
-    
-    _calculateCommission: function() {
-        // Commission calculation logic here
-        console.log('Calculating commission...');
-    }
-});
 
-return CommissionLinesWidget;
+    start: function () {
+      var self = this;
+      return this._super().then(function () {
+        self._bindEvents();
+      });
+    },
 
+    _bindEvents: function () {
+      var self = this;
+      this.$(".commission-calculate").on("click", function () {
+        self._calculateCommission();
+      });
+    },
+
+    _calculateCommission: function () {
+      // Commission calculation logic here
+      console.log("Calculating commission...");
+    },
+  });
+
+  return CommissionLinesWidget;
 });
